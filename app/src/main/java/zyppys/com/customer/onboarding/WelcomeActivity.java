cea55444b8ff1d9c2fd2b00fbc0005e5892b1f58
Shivakumar Burugu <burugu.shiva@gmail.com>
2017-04-13 20:38:18 +0530

    Added welcome layout

---
+++ app/src/main/java/zyppys/com/customer/onboarding/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package zyppys.com.customer.onboarding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import zyppys.com.zyppys.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
