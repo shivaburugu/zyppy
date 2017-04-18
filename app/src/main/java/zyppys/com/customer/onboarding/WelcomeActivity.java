3fe9177ad011114da52c0ee0f5fce4ccd9b9cca7
Shivakumar Burugu <shivakumar.burugu@cigna.com>
2017-04-18 23:45:26 +0530

    Added layout for landing page top section

--- app/src/main/java/zyppys/com/customer/onboarding/WelcomeActivity.java
+++ app/src/main/java/zyppys/com/customer/onboarding/WelcomeActivity.java
@@ -1,16 +1,35 @@
 package zyppys.com.customer.onboarding;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import zyppys.com.customer.R;
+import zyppys.com.customer.landing.LandingActivity;
 
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private Button enable_locaiton;
+    private TextView skip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        enable_locaiton = (Button) findViewById(R.id.enable_location);
+        skip = (TextView) findViewById(R.id.skip);
+        enable_locaiton.setOnClickListener(navigateToLandingPage);
+        skip.setOnClickListener(navigateToLandingPage);
     }
+
+    private View.OnClickListener navigateToLandingPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(WelcomeActivity.this,LandingActivity.class);
+            startActivity(intent);
+        }
+    };
 }
